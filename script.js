6cb7fe0053d4cfb15ca40febaa856785d13a82fe
sau-mili <146661411+sau-mili@users.noreply.github.com>
2024-02-14 00:15:48 +0530

    more

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event){
+    event.preventDefault(); 
+    
+    
+    document.getElementById("successMessage").style.display = "block";
+  });
+  
